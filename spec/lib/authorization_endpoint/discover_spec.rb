66351428626b601f40fadb331c39bfd31858ecb3
Jason Garber <jason@sixtwothree.org>
2018-07-17 18:26:06 -0400

    Add specs

---
+++ spec/lib/authorization_endpoint/discover_spec.rb
@@ -0,0 +1,9 @@
+describe AuthorizationEndpoint::Discover do
+  context 'when not given an HTTP::Response' do
+    let(:message) { 'response must be an HTTP::Response (given NilClass)' }
+
+    it 'raises an ArgumentError' do
+      expect { described_class.new(nil) }.to raise_error(AuthorizationEndpoint::ArgumentError, message)
+    end
+  end
+end
